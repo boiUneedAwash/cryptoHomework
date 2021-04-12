3b7f8998b1a1ea33947e6cabe49dbd2805139c41
boiUneedAwash <37860709+boiUneedAwash@users.noreply.github.com>
2021-04-13 09:00:17 +1200

    Add files via upload

---
+++ exercises/01_print.py
@@ -0,0 +1,12 @@
+# Using the print() function. Lines that begin with # are "comments" and are ignored by Python. They're used to tell other programmers (or yourself) what you want the code to do -- not necessarily what the code actually does.
+
+
+print('hello')
+
+print(123)
+
+print(42.0324)
+
+print(1 + 2)  # Comments can also go at the end of a line.
+
+print('hello' + 'world')  # Python will join these together, but we'll use a better way to do this later on.
---
+++ exercises/04_datatypes.py
@@ -0,0 +1,28 @@
+# We can check something's data type with the type() function.
+
+print('These are integers:')
+type(123)
+type(33)
+type(2 + 5)
+
+print('These are boolean (true or false) values:')
+type(True)
+type(False)
+type(True == False)
+
+print('These are floating-point values:')
+type(24.12)
+type(562.124432 - 2.9)
+
+print('These are strings')
+type('this is a string')
+type('also a string')
+type('x')
+
+# Python supports E-notation: https://en.wikipedia.org/wiki/Scientific_notation#E_notation
+print('The number below is 1000')
+print(1e3)
+print('The number below is 0.001')
+print(1e-3)
+
+
